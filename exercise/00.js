28e9ba8c025579151a8acfbbceb985a336eaa280
april sikes <aprilsikes@gmail.com>
2016-01-27 11:47:28 -0700

    completed

--- exercise/00.js
+++ exercise/00.js
@@ -1,7 +1,7 @@
 var file = require('./../promises/fs-promises');
 
-file.readFile(__dirname + '/../main.js', 'utf8', function (main) {
-  file.writeFile(__dirname + '/../build/main.js', main, 'utf8', function (result) {
+file.readFile(__dirname + '/../main.js', 'utf8').then(function (main) {
+  file.writeFile(__dirname + '/../build/main.js', main, 'utf8').then(function (result) {
     console.log(result);
   })
 })
--- exercise/04.js
+++ exercise/04.js
@@ -1,26 +1,34 @@
 var file = require('./../promises/fs-promises');
 
-var main = file.readFile(__dirname + '/../main.js', 'utf8'),
-    print = file.readFile(__dirname + '/../print.js', 'utf8'),
-    logic = file.readFile(__dirname + '/../logic.js', 'utf8'),
-    error = file.readFile(__dirname + '/../error.js', 'utf8');
 
 // ------------------------------------------------------
 //Edit the code below to do a Promise.all()
 // ------------------------------------------------------
 
-file.readFile(__dirname + '/../main.js', 'utf8').then(function (mainjs) {
-  main = mainjs
-  return file.readFile(__dirname + '/../print.js', 'utf8')
-}).then(function (printjs) {
-  print = printjs
-  return file.readFile(__dirname + '/../logic.js', 'utf8')
-}).then(function (logicjs) {
-  logic = logicjs
-  return file.readFile(__dirname + '/../error.js', 'utf8')
-}).then(function (errorjs) {
-  error = errorjs
-  return file.writeFile(__dirname + '/../build/app.js', main + print + logic + error, 'utf8')
-}).then(function (result) {
-  console.log(result);
-});
+// file.readFile(__dirname + '/../main.js', 'utf8').then(function (mainjs) {
+//   main = mainjs
+//   return file.readFile(__dirname + '/../print.js', 'utf8')
+// }).then(function (printjs) {
+//   print = printjs
+//   return file.readFile(__dirname + '/../logic.js', 'utf8')
+// }).then(function (logicjs) {
+//   logic = logicjs
+//   return file.readFile(__dirname + '/../error.js', 'utf8')
+// }).then(function (errorjs) {
+//   error = errorjs
+//   return file.writeFile(__dirname + '/../build/app.js', main + print + logic + error, 'utf8')
+// }).then(function (result) {
+//   console.log(result);
+// });
+
+var main = file.readFile(__dirname + '/../main.js', 'utf8'),
+print = file.readFile(__dirname + '/../print.js', 'utf8'),
+logic = file.readFile(__dirname + '/../logic.js', 'utf8'),
+error = file.readFile(__dirname + '/../error.js', 'utf8');
+
+Promise.all([main, print, logic, error]).then(function (files) {
+  return file.writeFile(__dirname + '/../build/app.js', files.join(''), 'utf8').then(function (results) {
+    console.log(results);
+  })
+
+})
